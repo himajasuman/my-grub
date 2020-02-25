0d13456c84f1c4767abe5b6a67778bca56c437c9
Himaja <himaja.ruttala@coxautoinc.com>
2020-02-24 20:49:09 -0800

    add dummy data, restraunt components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,25 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import restaurantsData from '../data';
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import './index.css';
 
+const restaurants = restaurantsData.restaurants;
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="restaurant-list">
+      {restaurants.map(({restaurant}) => (
+        <div className="restaurant-container" key={`restaurant${restaurant.id}`}>
+          <span className="user-rating">{restaurant.user_rating.aggregate_rating}</span>
+          <Link to={`/restaurant/?id=${restaurant.id}`}>
+            <img src={restaurant.thumb} />
+          </Link>
+          <p>
+            {restaurant.name}
+          </p>
+        </div>
+      ))}
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
